8a9bb66804801fea5cad3236c3c2bd3865aaad6f
Samarna <63645732+Samarna@users.noreply.github.com>
2021-05-05 20:36:48 +0530

    Add files via upload

---
+++ screens/WriteStoryScreen.js
@@ -0,0 +1,146 @@
+import React from 'react';
+import {Text,View,TouchableOpacity,StyleSheet,KeyboardAvoidingView,Image} from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
+
+export default class WriteStoryScreen extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            title: '',
+            author: '',
+            storyText: '',
+        }
+    }
+    render(){
+        return(
+            /*
+            <View style = {styles.container}>
+                <View>
+                    <Text style = {{textAlign:'center', fontSize:30}}>Write Story</Text>
+                </View>
+                <TextInput style={styles.inputBox}></TextInput>
+                <TextInput style={styles.inputBox}></TextInput>
+                <TextInput style={styles.storyBox}></TextInput>
+                <TouchableOpacity style = {styles.submitButton}>
+                    <Text style = {styles.submitButtonText}>Submit</Text>
+                </TouchableOpacity>
+            </View>
+            */
+            <KeyboardAvoidingView style={styles.container}
+            behavior="padding" enabled>
+               <Text>Story Hub</Text>
+               <TextInput 
+                   placeholder="Story Title"
+                   onChangeText= {(text)=>{
+                       this.setState({
+                           title: text
+                       })
+                   }}
+                   placeholderTextColor='black'
+                   value={this.state.title}
+                   style={styles.title}/>
+               <TextInput
+                   placeholder="Author"
+                   onChangeText= {(text)=>{
+                       this.setState({
+                           author: text
+                       })
+                   }}
+                   placeholderTextColor='black'
+                   value={this.state.author}
+                   style={styles.author} />
+               <TextInput 
+                   placeholder="Write your story"
+                   onChangeText= {(text)=>{
+                       this.setState({
+                           storyText: text
+                       })
+                   }}
+                   placeholderTextColor='black'
+                   value={this.state.storyText}
+                   style={styles.storyBox}
+                   multiline={true}/>
+               
+               <TouchableOpacity
+                   style={styles.submitButton}
+                   onPress={this.submitStory}
+                   >
+                   <Text style={styles.buttonText}>Submit</Text>
+               </TouchableOpacity>
+           </KeyboardAvoidingView>
+        )
+    }
+}
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+    },
+    displayText :{
+        fontSize :15,
+        textDecorationLine: "underline",
+    },
+    inputBox :{
+        width:300,
+        height:40,
+        borderWidth:1.5,
+        fontSize:20,
+        marginTop : 25,
+    },
+    storyBox :{
+        width:300,
+        height:200,
+        borderWidth:1.5,
+        fontSize:20,
+        marginTop : 25,
+        marginBottom : 25,
+    },
+    submitButton :{
+        backgroundColor : '#FBC02D',
+        width : 100,
+        height : 40,
+    },
+    submitButtonText :{
+        textAlign : 'center',
+        fontSize : 16,
+        fontWeight : 'bold',
+        color : 'white',
+        padding : 6,
+    },
+    title:{
+        height: 40,
+        borderWidth: 2,
+        marginTop: 40,
+        margin: 10,
+        color:'black',
+        padding: 6,
+  
+    },
+    author: {
+        height: 40,
+        borderWidth: 2,
+        margin: 10,
+         padding: 6,
+    },
+    storyText: {
+        height: 250,
+        borderWidth: 2,
+        margin: 10, 
+        padding: 6,
+        textAlignVertical: "top"
+    },
+    submitButton:{
+        justifyContent: 'center',
+        alignSelf: 'center',
+        backgroundColor: 'pink',
+        width: 80,
+        height: 40,color:'black',
+    },
+    buttonText: {
+        textAlign: 'center',
+        color: 'white',
+        fontWeight: 'bold',
+        color:'black',
+    }
+})
